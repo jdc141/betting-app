eb526946852c073ae1ee462ef9672677a3038d58
jdc141 <joecharland14@gmail.com>
2022-11-01 13:32:46 -0400

    added temporary rendoring of all teams

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    con = sqlite3.connect("nfl.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT name FROM team")
+
+    rows = cur.fetchall()
+    return render_template("index.html", rows = rows)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="81")
