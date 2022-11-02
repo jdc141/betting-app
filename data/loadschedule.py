a0984f80edb014c23db45a5de2300d7879abfbf6
jdc141 <joecharland14@gmail.com>
2022-11-01 21:24:04 -0400

    added data to schedule.db

--- data/loadschedule.py
+++ data/loadschedule.py
@@ -22,3 +22,27 @@ response = requests.request(
     "GET", url, headers=headers, params=querystring)
 json_response = response.json()
 
+cur.execute("SELECT * FROM team")
+rows = cur.fetchall()
+# print(rows)
+
+home_team = None
+away_team = None
+for item in json_response:
+    for row in rows:
+        if item['home_team'] in row[1]:
+            home_team_id = row[0]
+            home_team_name = item['home_team']
+            home_team_spread = item['bookmakers'][0]['markets'][0]['outcomes'][0]['point']
+            home_team_spread_price = item['bookmakers'][0]['markets'][0]['outcomes'][0]['price']
+        if item['away_team'] in row[1]:
+            away_team_id = row[0]
+            away_team_name = item['away_team']
+            away_team_spread= item['bookmakers'][0]['markets'][0]['outcomes'][1]['point']
+            away_team_spread_price = item['bookmakers'][0]['markets'][0]['outcomes'][1]['price']
+            total = item['bookmakers'][0]['markets'][1]['outcomes'][0]['point']
+    cur.execute("INSERT INTO schedule (home_team_id, away_team_id) VALUES (?,?)", (home_team, away_team))
+
+con.commit()
+
+
